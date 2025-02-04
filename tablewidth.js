d9a6c6b17226c5af612b37cf58fd0e5881adaf0a
Jeff Lange <lange.j.r@gmail.com>
2025-02-04 11:35:27 -0600

    removed console log

--- tablewidth.js
+++ tablewidth.js
@@ -5,10 +5,8 @@ function checkTableWidth(tableSelector, threshold = 625) {
 		tables.forEach((table, index) => {
 			const tableWidth = table.offsetWidth;
 			if (tableWidth > threshold) {
-				console.log(`Table ${index + 1} is wider than threshold, adding wide-table class`);
 				table.classList.add('wide-table');
 			} else {
-				console.log(`Table ${index + 1} is narrower than threshold, removing wide-table class`);
 				table.classList.remove('wide-table');
 			}
 		});
