edbc7d349a4d2ff906ca8c3dcd3a4948057a302c
Jeff Lange <lange.j.r@gmail.com>
2025-02-08 15:19:53 -0600

    new javascript for mobile tables validated, removing console logs

--- tablewidth.js
+++ tablewidth.js
@@ -9,22 +9,15 @@ function checkTableWidth(tableSelector) {
 			const tableWidth = table.offsetWidth;
 			const containerWidth = wrapper.parentElement.offsetWidth;
 			
-			console.log('Table width:', tableWidth);
-			console.log('Viewport width:', viewportWidth);
-			console.log('Container width:', containerWidth);
-			
 			if (tableWidth > containerWidth) {
 				if (tableWidth <= viewportWidth * 0.95) {  // Using 95% of viewport as threshold
-					console.log('Table fits viewport - using wide-table class');
 					wrapper.classList.add('wide-table');
 					wrapper.classList.remove('mobile-table');
 				} else {
-					console.log('Table exceeds viewport - using mobile-table class');
 					wrapper.classList.add('mobile-table');
 					wrapper.classList.remove('wide-table');
 				}
 			} else {
-				console.log('Table fits container - removing both classes');
 				wrapper.classList.remove('wide-table');
 				wrapper.classList.remove('mobile-table');
 			}
@@ -34,7 +27,6 @@ function checkTableWidth(tableSelector) {
 
 // Run on page load
 document.addEventListener('DOMContentLoaded', () => {
-	console.log('DOM loaded - checking tables');
 	checkTableWidth('.table-wrapper');
 });
 
@@ -43,7 +35,6 @@ let resizeTimer;
 window.addEventListener('resize', () => {
 	clearTimeout(resizeTimer);
 	resizeTimer = setTimeout(() => {
-		console.log('Window resized - checking tables');
 		checkTableWidth('.table-wrapper');
 	}, 50);
 });
